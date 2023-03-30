aa45591ec079a065931ee30d14a9dd2f78193ff8
Abrakadabra's <75881051+bafvfv@users.noreply.github.com>
2023-03-30 11:58:36 +0500

    Update keyboards.js

--- keyboards.js
+++ keyboards.js
@@ -1,11 +1,11 @@
-const { Markup, Extra} = require('opengram');
+const { Markup, Extra } = require('opengram');
 
-const taxiKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🚕 TAXI', 'taxi')]).extra()//, Markup.callbackButton('🚫 BACK', 'back')]).extra();
+const taxiKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🚕 TAXI', 'taxi')]).extra()
 
-const deliveryKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🛵 DELIVERY', 'dlvr')]).extra()//, Markup.callbackButton('🚫 BACK', 'back')]).extra();
+const deliveryKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🛵 DELIVERY', 'dlvr')]).extra()
 
-const vanKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🚌 MINIVAN', 'mvan')]).extra()//, Markup.callbackButton('🚫 BACK', 'back')]).extra();
+const vanKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🚌 MINIVAN', 'mvan')]).extra()
 
-const cargoKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🚚 CARGO', 'crgo')]).extra()//, Markup.callbackButton('🚫 BACK', 'back')]).extra();
+const cargoKeyboard = Markup.inlineKeyboard([Markup.callbackButton('🚚 CARGO', 'crgo')]).extra()
 
 module.exports = {taxiKeyboard, deliveryKeyboard, vanKeyboard, cargoKeyboard};
